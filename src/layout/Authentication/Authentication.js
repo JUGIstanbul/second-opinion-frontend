76d5fdb6ed970ec366472edb1f9a4f8bcfa77996
bhdryrdm <bhdryrdm@gmail.com>
2017-11-25 23:44:05 +0300

    Authentication Page and Login Component were added.

--- src/layout/Authentication/Authentication.js
+++ src/layout/Authentication/Authentication.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './Authentication.css';
+import Login from '../../pages/Authentication/Login';
 
 export default function Authentication({ match }) {
   return (
 
           <Switch>
-            <Route path={`${match.url}/login`} component={<div>Test</div>} />
-            <Route path={`${match.url}/register`} component={<div>Test</div>} />
-            <Route path={`${match.url}/reset`} component={<div>Test</div>} />
-            <Route path={`${match.url}/set-password`} component={<div>Test</div>} />
+            <Route path={`${match.url}/login`} component={Login} />
+            <Route path={`${match.url}/register`} component={() => <div>Fuck you fuck!!</div>} />
+            <Route path={`${match.url}/reset`} component={() => <div>Fuck you fuck!!</div>} />
+            <Route path={`${match.url}/set-password`} component={() => <div>Fuck you fuck!!</div>} />
             <Route
               path={match.url}
               render={() => <Redirect to="/auth/login" />}
