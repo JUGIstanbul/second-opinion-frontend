02779b8a2cb31e910dc5805c3ab437eb00a511c0
mikdatdogru <mikdatdogru@gmail.com>
2017-11-26 15:38:31 +0300

    create case state prepeared

--- src/pages/patient/CreateCase/CreateCase.js
+++ src/pages/patient/CreateCase/CreateCase.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class CreateCase extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          patientId: 123,
+          nickname: "string",
+          illnessStartDate: "Date",
+          symptoms: "string",
+          note: "string",
+          treatments: [
+            {
+              description: "string",
+              media: [
+                { id: 123 }
+              ]
+            }
+          ],
+          medicine: [
+            { id: 123 }
+          ]
+        }
+        }
+      }
+
+
+
   render() {
     return (
       <div>
         Create Case
+
+
+
+
+
+
+
       </div>
     );
   }
